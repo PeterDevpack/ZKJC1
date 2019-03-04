6014304328a7d17492ca3da744c660466042ce35
PeterDevpack <47619602+PeterDevpack@users.noreply.github.com>
2019-03-04 11:23:03 +0800

    Create chanpgncd.sql

---
+++ chanpgncd.sql
@@ -0,0 +1,118 @@
+prompt Importing table chanpgncd...
+set feedback off
+set define off
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('00029', '用户维护报告', 'orgCheckLog_huif.do?method=forward', 9, '50000', '69', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('60005', '汇总报告', 'forwordbatchsealchecklog_huif.do', 9, '60000', null, '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('00028', '印章查询报表', 'sealqueryReport_huif.do?method=view', 9, '50000', null, '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('60004', '通过率', 'accountPassRate_huif.do?method=view', 9, '60000', '121', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('60006', '交易明细报告', 'forwordvoucherchecklog_huif.do', 9, '60000', null, '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('10006', '账户销户与恢复', 'accountinfo.do?method=coverAccountForNetView', 9, '40000', '55', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('00007', '账户作废', 'accountinfo.do?method=physicsDeleteForNetForView', 9, '40000', '56', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('u-01', '账户高级查询', 'zhanghgjcx.do?method=enter', 9, 'tjcx1', '112', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('50000', '日志查询', null, 5, '1', null, '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('60000', '统计分析', null, 6, '1', null, '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('70000', '系统管理', null, 7, '1', null, '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('30000', '机构管理', 'main/org_manager.jsp', 9, '70000', '77', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('30000', '添加', null, 9, '30000', '87', '1', null, '1', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('30000', '删除', null, 9, '30000', '88', '1', null, '2', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('30000', '导入', null, 9, '30000', '122', '1', null, '6', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('30000', '导出', null, 9, '30000', '123', '1', null, '7', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('30000', '修改', null, 9, '30000', '89', '1', null, '3', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('37000', '角色管理', 'roleManager.do?method=list', 2, '70000', '125', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('31000', '柜员管理', 'main/clerk_manager.jsp', 2, '70000', '80', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('31000', '添加', null, 9, '31000', '98', '1', null, '1', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('31000', '修改', null, 9, '31000', '99', '1', null, '2', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('31000', '删除', null, 9, '31000', '100', '1', null, '3', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('31000', '导出', null, 9, '31000', '121', '1', null, '5', '1', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('11110', '印章查阅', null, 1, '1', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('11111', '印鉴建库', 'accountinfo.do?method=openAccount', 11, '11110', '0', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('11112', '印鉴变更', 'accountinfo.do?method=changeForword', 12, '11110', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('11113', '资料修改', 'accountinfo.do?method=changeAccountInfo', 13, '11110', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('22220', '印章核验', null, 2, '1', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('22221', '印章核验', 'accountinfo.do?method=accInfoCheck', 9, '22220', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('11114', '印章查阅', 'accountinfo.do?method=yinjxxck', 14, '11110', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('38000', '密码重置', 'clerkManage.do?method=forwardReset', 9, '70000', '60', '1', null, '0', '0', null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('32123', '授权权限', 'shuangqts.jsp', 9, '99770', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('10007', '账户挂失与解挂', 'accountinfo.do?method=zhanghgsView', 9, '40000', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('10009', '账户关联与解关联', 'accountinfo.do?method=accountLink', 9, '40000', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('00021', '验印', 'qiantyy.do?method=accInfoCheck', 9, '99880', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('00990', '批量验印', 'pilyy.do?method=view', 9, '99880', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('10008', '账户冻结与解冻', 'accountinfo.do?method=zhanghdjView', 9, '40000', '0', '1', null, '0', null, null);
+
+insert into chanpgncd (GONGNID, GONGNMC, GONGNURL, GONGNSX, SHANGJGN, QUANXWZ, ZHUANGT, BEIZ, ZIGNID, GONGNLX, CLASSID)
+values ('22222', '待审任务', 'accountinfo.do?method=getUnCheckTasks', 1, '22220', '0', '1', null, '0', null, null);
+
+prompt Done.
